548dda2a216cf94855cfb403ae1ff814ff6c0b9a
AdilTestelin <adil.testelin59@hotmail.fr>
2020-05-07 07:21:01 +0200

    My test works, but will it pass Leetcode compilator ? (spoiler : no)

--- src/addTwoNumbersPackage/addTwoNumbers.java
+++ src/addTwoNumbersPackage/addTwoNumbers.java
@@ -10,18 +10,18 @@ public class addTwoNumbers {
         return nbr;
     }
 
-    public static int addTwoNumbersMethod(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbersMethod(ListNode l1, ListNode l2) {
         ListNode[] listnode = new ListNode[1000];
         int exp = 1;
         int r1 = l1.val;
-        System.out.println("Valeur du noeud courant l1 : " + l1.val);
+        // System.out.println("Valeur du noeud courant l1 : " + l1.val);
         int r2 = l2.val;
 
         while(l1.next != null) {
             l1 = l1.next;
             r1 += l1.val * power(exp);
             exp++;
-            System.out.println("Valeur du noeud courant l2 : " + l1.val);
+            // System.out.println("Valeur du noeud courant l2 : " + l1.val);
         }
 
         exp = 1;
@@ -29,16 +29,21 @@ public class addTwoNumbers {
             l2 = l2.next;
             r2 += l2.val * power(exp);
             exp++;
-            System.out.println("Valeur du noeud courant l2 : " + l2.val);
+            // System.out.println("Valeur du noeud courant l2 : " + l2.val);
         }
         int r3 = r1 + r2;
 
         int i = 0;
         listnode[i] = new ListNode(r3%10, listnode[i+1]);
+        r3 /= 10;
+        System.out.println("Valeur du noeud courant r3 :" + listnode[i].val);
         while (r3 != 0){
-            //
+            listnode[i] = new ListNode(r3%10, listnode[i+1]);
+            System.out.println("Valeur du noeud courant r3 :" + listnode[i].val);
+            r3 /= 10;
         }
-        return r3;
+        listnode[i].next = null;
+        return listnode[0];
     }
 
 
