2eee0d200b74a1fa41657743f9bf82fd115b837a
AdilTestelin <adil.testelin59@hotmail.fr>
2020-05-07 07:12:52 +0200

    Testing bizarre things at dawn

--- src/addTwoNumbersPackage/addTwoNumbers.java
+++ src/addTwoNumbersPackage/addTwoNumbers.java
@@ -10,21 +10,35 @@ public class addTwoNumbers {
         return nbr;
     }
 
-    public static int addTwoNumbersMethod(ListNode l1) {
-        int exp = 0;
-        int r1 = 0;
-        int r2 = 0;
+    public static int addTwoNumbersMethod(ListNode l1, ListNode l2) {
+        ListNode[] listnode = new ListNode[1000];
+        int exp = 1;
+        int r1 = l1.val;
+        System.out.println("Valeur du noeud courant l1 : " + l1.val);
+        int r2 = l2.val;
 
         while(l1.next != null) {
-            if(exp > 0) {
-                r1 += l1.val * power(exp);
-            } else {
-                r1 += l1.val;
-            }
-            exp++;
             l1 = l1.next;
+            r1 += l1.val * power(exp);
+            exp++;
+            System.out.println("Valeur du noeud courant l2 : " + l1.val);
         }
-        return r1;
+
+        exp = 1;
+        while(l2.next != null) {
+            l2 = l2.next;
+            r2 += l2.val * power(exp);
+            exp++;
+            System.out.println("Valeur du noeud courant l2 : " + l2.val);
+        }
+        int r3 = r1 + r2;
+
+        int i = 0;
+        listnode[i] = new ListNode(r3%10, listnode[i+1]);
+        while (r3 != 0){
+            //
+        }
+        return r3;
     }
 
 
@@ -33,6 +47,10 @@ public class addTwoNumbers {
         ListNode l2 = new ListNode(4, l3);
         ListNode l1 = new ListNode(2, l2);
 
-        System.out.println(addTwoNumbersMethod(l1));
+        ListNode l6 = new ListNode(4, null);
+        ListNode l5 = new ListNode(6, l6);
+        ListNode l4 = new ListNode(5, l5);
+
+        System.out.println(addTwoNumbersMethod(l1, l4));
     }
 }
