e25b7a75882019049d22d6ce55e18e7554c2fac6
testelin <adil.testelin.etu@univ-lille.fr>
2020-05-09 02:09:58 +0200

    Beggining of reflexion, main problem : knowing if a string is already in the substring or not. Need to reflect on it

--- src/LongestSubWoutRepeatingCharac/Solution.java
+++ src/LongestSubWoutRepeatingCharac/Solution.java
@@ -4,13 +4,18 @@ public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int cpt = 1;
         int cptFinal = 1;
-        for(int i = 0; i < s.length()-1; i++){
-            if(s.charAt(i) == s.charAt(i+1)){
-                cpt++;
-            } else {
-                cptFinal = cpt;
+        for (int i = 0; i < s.length(); i++) {
+            String tab =  "";
+            tab += s.charAt(i);
+            for (int j = i; j < s.length(); j++) {
+                char nextString = s.charAt(j+1);
+                if (s.charAt(i) == s.charAt(i + 1) && !tab.contains(nextString)) {
+                    cpt++;
+                } else {
+                    cptFinal = cpt;
+                }
                 cpt = 1;
             }
         } return cptFinal;
-    } 
+    }
 }
