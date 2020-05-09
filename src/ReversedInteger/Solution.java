7cada772a51e133365300c6d16f8a130a97747d5
AdilTestelin <adil.testelin59@hotmail.fr>
2020-05-09 21:02:45 +0200

    Beginning of the reversed integer challenge

---
+++ src/ReversedInteger/Solution.java
@@ -0,0 +1,13 @@
+package ReversedInteger;
+
+public class Solution {
+
+    public static int reverse(int x) {
+        int res = 0;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+    }
+}
